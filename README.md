b1c38d5773f3a1541bbd82821aa103098026190a
gia-bartlett <g.bartlett89@gmail.com>
2020-06-30 17:22:33 +0100

    First Python commit

---
+++ README.md
@@ -0,0 +1,19 @@
+Python Notes
+
+# to comment in Python
+select all CTRL / to comment multiple lines out
+
+Variables:
+
+x = 10 (integer)
+y = 3.5 (float)
+name = "James" (string)
+
+print(x) (will print 10)
+
+x and y are not good naming conventions. Be more descriptive with names of variables
+Include comments - future you with appreciate it!
+
+Methods:
+cast - turn int into string
+
---
+++ variables.py
@@ -0,0 +1,50 @@
+# print("Hello, World!")
+#
+# # Let's create a variable
+#
+# my_age = 31
+# hector_age = 14.8
+# name = "Georgina"
+#
+# print(my_age)
+# print(hector_age)
+# print(name)
+# print(hector_age * 7)
+# print(my_age)
+#
+# # built in method called 'type
+#
+# print(type(my_age))
+#
+# print(str(my_age) + name)
+#turning an int into a string is called casting
+
+# name = input("Please print your name ")
+# age = input("Please enter your age ")
+# print(name)
+# print(age)
+
+#Overwriting a variable
+
+# name = "James"
+# print(name)
+# name = "Bond"
+# print(name)
+
+#Python runs top to bottom so name will be Bond in this example
+
+#exercise
+#create a vairable called first_name and last_name
+#create a variable called full_name adn display full name
+
+first_name = input("Please print your first name ")
+last_name = input("Please print your last name ")
+age = input("Please print your age ")
+address = input("Please print your address ")
+
+full_name = first_name + " " + last_name
+
+print(full_name)
+print(age)
+print(address)
+
